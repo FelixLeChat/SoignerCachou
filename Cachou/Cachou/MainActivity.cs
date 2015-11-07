57ab310b7f4fbd9c457758e723db2a0d4f9da1b9
Alex <alex_gagne94@hotmail.com>
2015-11-07 16:56:36 -0500

    Debut de drag and drop

--- Cachou/Cachou/MainActivity.cs
+++ Cachou/Cachou/MainActivity.cs
@@ -1,7 +1,10 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.Widget;
+using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.OS;
+using Android.Views;
+using Android.Widget;
 
 namespace Cachou
 {
@@ -13,6 +16,8 @@ namespace Cachou
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+           // FindViewById<ImageView>(Resource.Id.nom_de_view)
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
@@ -27,6 +32,42 @@ namespace Cachou
         private void ChangeToMainView()
         {
             SetContentView(Resource.Layout.CachouMain);
+        }
+
+        private class ToolShadowBuilder : View.DragShadowBuilder
+        {
+            private Drawable shadow;
+
+            public ToolShadowBuilder(View v) : base(v)
+            {
+                // Nous permettons l'utilisation d'une cache pour dessiner
+                // L'ombre de notre icône
+                v.DrawingCacheEnabled = true;
+                Bitmap bm = v.DrawingCache;
+                shadow = new BitmapDrawable(bm);
+
+                // L'ombre devient un genre de gris
+                shadow.SetColorFilter(Color.ParseColor("#4EB1FB"), PorterDuff.Mode.Multiply);
+            }
+
+            public override void OnProvideShadowMetrics(Point size, Point touch)
+            {
+                // Nous prenons les dimensions de notre image
+                int width = View.Width;
+                int height = View.Height;
+                // Nous créons les dimensions de l'ombre
+                shadow.SetBounds(0, 0, width, height);
+                size.Set(width, height);
+
+                touch.Set(width / 2, height / 2);
+            }
+
+            public override void OnDrawShadow(Canvas canvas)
+            {
+                // Nous dessinons l'ombre de l'image
+                base.OnDrawShadow(canvas);
+                shadow.Draw(canvas);
+            }
         }
     }
 }
