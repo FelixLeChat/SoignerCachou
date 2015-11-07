e290c0664d191bff473b3161e41216c8f76cb0b9
FelixLeChat <felixlrc21@gmail.com>
2015-11-07 16:34:38 -0500

    user interface ++

--- Cachou/Cachou/MainActivity.cs
+++ Cachou/Cachou/MainActivity.cs
@@ -8,25 +8,36 @@ namespace Cachou
     [Activity(MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
-        int count = 1;
+        private static ImageView _cachouImageView;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.CachouMain);
+
+            SeekBar seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            seekBar.ProgressChanged += ChangeCachouMood;
+
+            _cachouImageView = FindViewById<ImageView>(Resource.Id.imageViewCachou);
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            /*Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate {ChangeToMainView(); };
+            button.Click += delegate {ChangeToMainView(); };*/
         }
 
         private void ChangeToMainView()
         {
             SetContentView(Resource.Layout.CachouMain);
+        }
+
+        public static void ChangeCachouMood(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            _cachouImageView.SetImageResource(Resource.Drawable.Cachou_Confu);
+
         }
     }
 }
--- Cachou/Cachou/Resources/Resource.Designer.cs
+++ Cachou/Cachou/Resources/Resource.Designer.cs
@@ -81,37 +81,40 @@ namespace Cachou
 			public const int Cachou_triste = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int heart = 2130837516;
+			public const int dossier = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int home = 2130837517;
+			public const int heart = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int Icon = 2130837518;
+			public const int home = 2130837518;
 			
 			// aapt resource value: 0x7f02000f
-			public const int interest = 2130837519;
+			public const int Icon = 2130837519;
 			
 			// aapt resource value: 0x7f020010
-			public const int justine = 2130837520;
+			public const int interest = 2130837520;
 			
 			// aapt resource value: 0x7f020011
-			public const int login_button = 2130837521;
+			public const int justine = 2130837521;
 			
 			// aapt resource value: 0x7f020012
-			public const int medecine = 2130837522;
+			public const int login_button = 2130837522;
 			
 			// aapt resource value: 0x7f020013
-			public const int mute = 2130837523;
+			public const int medecine = 2130837523;
 			
 			// aapt resource value: 0x7f020014
-			public const int name = 2130837524;
+			public const int mute = 2130837524;
 			
 			// aapt resource value: 0x7f020015
-			public const int siringe = 2130837525;
+			public const int name = 2130837525;
 			
 			// aapt resource value: 0x7f020016
-			public const int thermometer = 2130837526;
+			public const int siringe = 2130837526;
+			
+			// aapt resource value: 0x7f020017
+			public const int thermometer = 2130837527;
 			
 			static Drawable()
 			{
@@ -126,44 +129,38 @@ namespace Cachou
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int Home = 2131034113;
-			
-			// aapt resource value: 0x7f05000c
-			public const int MyButton = 2131034124;
-			
-			// aapt resource value: 0x7f050004
-			public const int Pause = 2131034116;
+			// aapt resource value: 0x7f05000a
+			public const int MyButton = 2131034122;
 			
 			// aapt resource value: 0x7f050002
-			public const int Sound = 2131034114;
-			
-			// aapt resource value: 0x7f050007
-			public const int button1 = 2131034119;
-			
-			// aapt resource value: 0x7f05000b
-			public const int editText1 = 2131034123;
+			public const int Pause = 2131034114;
 			
 			// aapt resource value: 0x7f050009
-			public const int editText2 = 2131034121;
-			
-			// aapt resource value: 0x7f050005
-			public const int imageView1 = 2131034117;
+			public const int editText1 = 2131034121;
 			
 			// aapt resource value: 0x7f050008
-			public const int imageView2 = 2131034120;
+			public const int editText2 = 2131034120;
+			
+			// aapt resource value: 0x7f050006
+			public const int imageView2 = 2131034118;
+			
+			// aapt resource value: 0x7f050001
+			public const int imageView3 = 2131034113;
 			
 			// aapt resource value: 0x7f050003
-			public const int imageView3 = 2131034115;
+			public const int imageViewCachou = 2131034115;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout1 = 2131034112;
 			
-			// aapt resource value: 0x7f050006
-			public const int textView1 = 2131034118;
+			// aapt resource value: 0x7f050005
+			public const int seekBar = 2131034117;
 			
-			// aapt resource value: 0x7f05000a
-			public const int textView2 = 2131034122;
+			// aapt resource value: 0x7f050007
+			public const int textView1 = 2131034119;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView2 = 2131034116;
 			
 			static Id()
 			{
