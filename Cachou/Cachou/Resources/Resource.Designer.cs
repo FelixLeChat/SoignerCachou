e0e283503831a640d133b649ae5aa403df15871f
FelixLeChat <felixlrc21@gmail.com>
2015-11-07 14:34:44 -0500

    add png and ...

--- Cachou/Cachou/Resources/Resource.Designer.cs
+++ Cachou/Cachou/Resources/Resource.Designer.cs
@@ -57,43 +57,58 @@ namespace Cachou
 			public const int blood = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int Cachou = 2130837508;
+			public const int blue_bandaid = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int Cachou_Confu = 2130837509;
+			public const int blue_heart = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int Cachou_happy = 2130837510;
+			public const int blue_pills = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int Cachou_triste = 2130837511;
+			public const int blue_siringe = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int home = 2130837512;
+			public const int Cachou = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int Icon = 2130837513;
+			public const int Cachou_Confu = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int interest = 2130837514;
+			public const int Cachou_happy = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int login_button = 2130837515;
+			public const int Cachou_triste = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int medecine = 2130837516;
+			public const int heart = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int mute = 2130837517;
+			public const int home = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int name = 2130837518;
+			public const int Icon = 2130837518;
 			
 			// aapt resource value: 0x7f02000f
-			public const int siringe = 2130837519;
+			public const int interest = 2130837519;
 			
 			// aapt resource value: 0x7f020010
-			public const int thermometer = 2130837520;
+			public const int login_button = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int medecine = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int mute = 2130837522;
+			
+			// aapt resource value: 0x7f020013
+			public const int name = 2130837523;
+			
+			// aapt resource value: 0x7f020014
+			public const int siringe = 2130837524;
+			
+			// aapt resource value: 0x7f020015
+			public const int thermometer = 2130837525;
 			
 			static Drawable()
 			{
@@ -108,20 +123,41 @@ namespace Cachou
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int MyButton = 2131034116;
+			// aapt resource value: 0x7f050001
+			public const int Home = 2131034113;
+			
+			// aapt resource value: 0x7f05000b
+			public const int MyButton = 2131034123;
 			
 			// aapt resource value: 0x7f050003
-			public const int editText1 = 2131034115;
-			
-			// aapt resource value: 0x7f050000
-			public const int editText2 = 2131034112;
+			public const int Pause = 2131034115;
 			
 			// aapt resource value: 0x7f050002
-			public const int textView1 = 2131034114;
+			public const int Sound = 2131034114;
 			
-			// aapt resource value: 0x7f050001
-			public const int textView2 = 2131034113;
+			// aapt resource value: 0x7f050006
+			public const int button1 = 2131034118;
+			
+			// aapt resource value: 0x7f05000a
+			public const int editText1 = 2131034122;
+			
+			// aapt resource value: 0x7f050008
+			public const int editText2 = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int imageView1 = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int imageView2 = 2131034119;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView1 = 2131034117;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView2 = 2131034121;
 			
 			static Id()
 			{
--- Cachou/Cachou/obj/Debug/android/cachou/cachou/R.java
+++ Cachou/Cachou/obj/Debug/android/cachou/cachou/R.java
@@ -15,26 +15,38 @@ public final class R {
         public static final int background=0x7f020001;
         public static final int bandaid=0x7f020002;
         public static final int blood=0x7f020003;
-        public static final int cachou=0x7f020004;
-        public static final int cachou_confu=0x7f020005;
-        public static final int cachou_happy=0x7f020006;
-        public static final int cachou_triste=0x7f020007;
-        public static final int home=0x7f020008;
-        public static final int icon=0x7f020009;
-        public static final int interest=0x7f02000a;
-        public static final int login_button=0x7f02000b;
-        public static final int medecine=0x7f02000c;
-        public static final int mute=0x7f02000d;
-        public static final int name=0x7f02000e;
-        public static final int siringe=0x7f02000f;
-        public static final int thermometer=0x7f020010;
+        public static final int blue_bandaid=0x7f020004;
+        public static final int blue_heart=0x7f020005;
+        public static final int blue_pills=0x7f020006;
+        public static final int blue_siringe=0x7f020007;
+        public static final int cachou=0x7f020008;
+        public static final int cachou_confu=0x7f020009;
+        public static final int cachou_happy=0x7f02000a;
+        public static final int cachou_triste=0x7f02000b;
+        public static final int heart=0x7f02000c;
+        public static final int home=0x7f02000d;
+        public static final int icon=0x7f02000e;
+        public static final int interest=0x7f02000f;
+        public static final int login_button=0x7f020010;
+        public static final int medecine=0x7f020011;
+        public static final int mute=0x7f020012;
+        public static final int name=0x7f020013;
+        public static final int siringe=0x7f020014;
+        public static final int thermometer=0x7f020015;
     }
     public static final class id {
-        public static final int MyButton=0x7f050004;
-        public static final int editText1=0x7f050003;
-        public static final int editText2=0x7f050000;
-        public static final int textView1=0x7f050002;
-        public static final int textView2=0x7f050001;
+        public static final int Home=0x7f050001;
+        public static final int MyButton=0x7f05000b;
+        public static final int Pause=0x7f050003;
+        public static final int Sound=0x7f050002;
+        public static final int button1=0x7f050006;
+        public static final int editText1=0x7f05000a;
+        public static final int editText2=0x7f050008;
+        public static final int imageView1=0x7f050004;
+        public static final int imageView2=0x7f050007;
+        public static final int linearLayout1=0x7f050000;
+        public static final int textView1=0x7f050005;
+        public static final int textView2=0x7f050009;
     }
     public static final class layout {
         public static final int cachoumain=0x7f030000;
