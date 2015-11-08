a205019889986684446b3bcbc9bf1dca65c733e0
Alex <alex_gagne94@hotmail.com>
2015-11-08 09:04:46 -0500

    Shouldn't spam server anymore

--- Cachou/Cachou/WebAPI/WebmessageSender.cs
+++ Cachou/Cachou/WebAPI/WebmessageSender.cs
@@ -6,7 +6,6 @@ static class WebmessageSender
     public static void postServer(string message)
     {
         DateTime localDate = DateTime.Now;
-        string culture = "en-GB";
-        WebAPI.SendChildAction(localDate.ToString(culture), message);
+        WebAPI.SendChildAction(localDate.ToString("dd MMMM yyyy"), message);
     }
 }
